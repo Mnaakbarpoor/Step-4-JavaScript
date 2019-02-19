fff1d500565c37c261e003bbca1a2b06ba1f2000
monaakbarpoor <45979598+Mnaakbarpoor@users.noreply.github.com>
2019-02-19 06:41:43 -0800

    Jvasacript-week1-loop(for)

---
+++ loop.js
@@ -0,0 +1,34 @@
+
+console.log( ' square of number');
+
+let x = [1 , 2 , 3 , 4 , 5 , 100];
+
+for (let me = 1; me <= 100; me++) {
+    let y = [me*me ];
+    console.log(y)    
+}
+
+console.log('sum of number')
+
+sum = 1
+
+for (let i = 1; i <= 100; i++) {
+   let z = [i+1] ;
+    console.log(z)
+}
+
+console.log('sum of square of odd number')
+
+for(let b = 1; b <= 100; b++){
+    let L = [2*b-1]*[2*b-1];
+    console.log(L)
+}
+
+console.log('sum of even of square number')
+
+for(let c = 1; c <= 100; c++){
+    let G = [2*c]*[2*c];
+    console.log(G)
+}
+
+
