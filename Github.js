3789f3d507e6d73d0ac1167265d524f24b7f91bc
monaakbarpoor <45979598+Mnaakbarpoor@users.noreply.github.com>
2019-02-19 06:38:06 -0800

    Add files via upload

---
+++ Github.js
@@ -0,0 +1,261 @@
+// 1
+console.log('doroud, jahan! //Persian')
+
+console.log('hallo, welt! //German')
+
+console.log('anyeong, segye! //Korean')
+
+// 2
+
+// console.log('I'm awesome');
+
+console.log("I'm awsome")
+
+console.log('Im awesome');
+
+// 3.1
+
+let x;
+
+// 3.2
+
+console.log('the value of my variable x will be:whateverYouThinkItWillLog');
+
+// 3.3
+
+console.log(x)
+
+// 3.4
+
+x = 6
+
+// 3.5
+
+console.log("I think the type of x will be: ", typeof x);
+
+// 3.6
+
+console.log(x);
+
+// 4
+
+let y;'';
+
+// 4.1
+
+console.log("I think the value of y will be: ", typeof y);
+
+// 4.2
+
+console.log(y)
+
+// 4.3
+
+y = 'Hello';
+
+// 4.4
+
+console.log("I think the value of y will be: ", typeof y);
+
+// 4.5
+
+console.log(y)
+
+// 5
+
+// 5.1
+
+let z = 7.25;
+
+// 5.2
+
+console.log(z)
+
+// 5.3
+
+let a = Math.round(z)
+
+// 5.4
+
+console.log(a)
+
+// 5.5
+
+let e; 
+
+if (a > z){
+    e = a
+} else {
+    e = z
+}
+
+//5.6
+
+console.log(e)
+
+//6 arrays!
+
+//6.1
+
+let plurality = []
+
+// 6.2
+
+ console.log("I think ...");
+
+ //6.3
+
+ console.log(plurality)
+
+ //6.4
+
+ let MFA = ["panda"];
+
+ //6.5
+
+console.log(MFA)
+
+//6.6
+
+MFA.push('baby pig')
+
+//6.7
+
+console.log(MFA)
+
+// 7 more strings
+
+// 7.1
+
+let myString = 'this is a test'
+
+//7.2
+console.log(myString)
+
+//7.3
+
+myString.length;
+
+//7.4
+
+console.log(myString.length)
+
+//8
+
+// 8.1
+
+let name = 'Mona';
+let age = 17;
+let question = true;
+let you = {}
+
+//8.2
+
+console.log('the value of my variable name is: ', name);
+console.log('the value of my variable age is: ', age);
+console.log('thevalue of my variable question is: ', question);
+console.log('the value of my variablr you is:', you);
+
+//8.3
+
+console.log('the value of my variable name is: ',typeof name);
+console.log('the value of my variable age is: ',typeof age);
+console.log('thevalue of my variable question is: ',typeof question);
+console.log('the value of my variablr you is:',typeof you);
+
+//8.4
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof question);
+console.log(typeof you);
+
+//8.5
+
+if (name !== age){
+    console.log('different type')
+}else{
+    console.log('same type')
+}
+
+if (name !== question){
+    console.log('different type')
+}else{
+    console.log('same type')
+}
+
+if (name !== you){
+    console.log('different type')
+}else{
+    console.log('same type')
+}
+
+if (age !== question){
+    console.log('different type')
+}else{
+    console.log('same type')
+}
+
+
+if (age !== you){
+    console.log('different type')
+}else{
+    console.log('same type')
+}
+
+if (question !== you){
+    console.log('different type')
+}else{
+    console.log('same type')
+}
+
+//9
+
+
+
+//9.1
+
+let s = 25
+s = s % 2;
+console.log(s)
+
+let gh = 82
+gh = gh % 5
+console.log(gh)
+
+let fm = 214
+fm = fm % 12
+console.log(fm)
+
+console.log('when we write 214 % 12 , they devide and the answer is the left over  from subtract')
+
+//10
+
+//10.1
+
+console.log("You can store multiple types in an array!");
+
+let array = []
+
+array[1] = 'hello world'
+array[2] = 20
+array[3] = true
+
+console.log(array)
+
+// 10.2
+
+6/0 === 10/0;
+
+let y1 = 6/0;
+let y2 = 10/0;
+
+if (y1 === y2) {
+  console.log("true")
+} else {
+  console.log("false")
+}
+
+//  10.3
+
+console.log("The triple equals sign (===) is used to compare both the Type and the actual Value of a variable");
